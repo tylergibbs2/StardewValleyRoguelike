17fef4864745bb2b92432cdd8d34450dcb93cd0d
Tyler <gibbstyler7@gmail.com>
2022-09-03 10:12:39 -0400

    disables cinder shards being given on unforge

--- StardewRoguelike/Patches/FreeForgePatch.cs
+++ StardewRoguelike/Patches/FreeForgePatch.cs
@@ -1,4 +1,7 @@
-﻿using StardewValley.Menus;
+﻿using HarmonyLib;
+using StardewValley;
+using StardewValley.Menus;
+using System;
 
 namespace StardewRoguelike.Patches
 {
@@ -10,6 +13,32 @@ namespace StardewRoguelike.Patches
         {
             __result = 0;
             return false;
+        }
+    }
+
+    internal class FreeForgePatch2 : Patch
+    {
+        protected override PatchDescriptor GetPatchDescriptor() => new(typeof(ForgeMenu), "GetForgeCostAtLevel");
+
+        public static bool Prefix(ref int __result)
+        {
+            __result = 0;
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Utility), "CollectOrDrop", new Type[] { typeof(Item), typeof(int) })]
+    internal class UtilityCollectOrDrop
+    {
+        public static bool Prefix(ref bool __result, Item item, int direction)
+        {
+            if (item is not null && item.ParentSheetIndex == 848)
+            {
+                __result = false;
+                return false;
+            }
+
+            return true;
         }
     }
 }
