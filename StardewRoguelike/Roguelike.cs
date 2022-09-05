12bd82c78f05c82c42e6ed6f887b5393a2ff9e69
Tyler <gibbstyler7@gmail.com>
2022-09-05 09:16:56 -0500

    updates qi dialogue in lobby

--- StardewRoguelike/Roguelike.cs
+++ StardewRoguelike/Roguelike.cs
@@ -532,7 +532,8 @@ namespace StardewRoguelike
                 $"Below, you will also find vendors for perks and curses. View your active perks by pressing {perkKey}.#$b#" +
                 "Your tracking chip will keep note of all your statistics. You can view them by typing /stats. " +
                 "You can reset your journey at any time with the /reset command.#$b#" +
-                "Best of luck on the way down!"
+                "If you're up for, check out that statue over there for a more difficult time.#$b#" +
+                "Good luck, kid."
             );
         }
 
