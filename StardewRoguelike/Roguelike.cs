915820ac0dbc0f6719157c4c33cea20f19425159
Tyler <gibbstyler7@gmail.com>
2022-09-12 16:02:15 -0500

    changes floors descended to be based on highest level mineshaft

--- StardewRoguelike/Roguelike.cs
+++ StardewRoguelike/Roguelike.cs
@@ -253,10 +253,7 @@ namespace StardewRoguelike
         public static void ReturnedToTitle(object sender, ReturnedToTitleEventArgs e)
         {
             if (CurrentLevel != 0)
-            {
                 CurrentLevel--;
-                ModEntry.Stats.FloorsDescended--;
-            }
         }
 
         public static void TimeChanged(object sender, TimeChangedEventArgs e)
@@ -271,7 +268,7 @@ namespace StardewRoguelike
                 ModEntry.Stats.Reset();
 
             CurrentLevel++;
-            ModEntry.Stats.FloorsDescended++;
+            ModEntry.Stats.FloorsDescended = GetHighestMineShaftLevel();
             Merchant.CurrentShop = null;
             Perks.CurrentMenu = null;
 
