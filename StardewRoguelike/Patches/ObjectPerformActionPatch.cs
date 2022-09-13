9bc9d0470508dad2da98420fe79159b6ce54cc5a
Tyler <gibbstyler7@gmail.com>
2022-09-13 16:13:25 -0500

    disables destruction of the farm computer

---
+++ StardewRoguelike/Patches/ObjectPerformActionPatch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+
+namespace StardewRoguelike.Patches
+{
+    [HarmonyPatch(typeof(StardewValley.Object), "performToolAction")]
+    internal class ObjectPerformActionPatch
+    {
+        public static bool Prefix(StardewValley.Object __instance, ref bool __result)
+        {
+            // Farm Computer
+            if (__instance.ParentSheetIndex == 239)
+            {
+                __result = false;
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
