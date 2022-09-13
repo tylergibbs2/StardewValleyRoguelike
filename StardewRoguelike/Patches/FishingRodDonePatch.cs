0fb311ef36f28157dec0f62bb059366dced9b52d
Tyler <gibbstyler7@gmail.com>
2022-09-12 23:35:43 -0500

    makes fishing rods break after 3 uses

---
+++ StardewRoguelike/Patches/FishingRodDonePatch.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using StardewRoguelike.VirtualProperties;
+using StardewValley;
+using StardewValley.Tools;
+using System;
+using System.Reflection;
+
+namespace StardewRoguelike.Patches
+{
+    [HarmonyPatch(typeof(FishingRod), "doDoneFishing")]
+    internal class FishingRodDonePatch
+    {
+        public static void Postfix(FishingRod __instance)
+        {
+            Farmer lastUser = (Farmer)__instance.GetType().BaseType.GetField("lastUser", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(__instance);
+            if (lastUser is null || !lastUser.IsLocalPlayer)
+                return;
+
+            __instance.increment_FishingRodUses();
+
+            if (__instance.get_FishingRodUses() >= 3)
+            {
+                Game1.player.removeItemFromInventory(__instance);
+                Game1.playSound("breakingGlass");
+            }
+        }
+    }
+}
